c2f1e774c3633e1681ca7dcd242217fa268f2992
Tova Greene <tovalgreene@gmail.com>
2023-11-05 13:56:34 -0500

    edited webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -33,7 +33,7 @@ module.exports = () => {
         publicPath: '/',
         icons: [
           {
-            src: path.resolve(__dirname, 'client/src/images/logo.png'),
+            src: path.resolve(__dirname, './client/src/images/logo.png'),
             sizes: [96, 128, 192, 256, 384, 512],
             destination: path.join('assets', 'icons'),
           },
